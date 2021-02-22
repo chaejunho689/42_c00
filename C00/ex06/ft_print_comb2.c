87fd492b3e72c56e8dfa0244ab7b6a2a61d60b6d
chaejunho689 <chaejunho689@gmail.com>
2021-02-22 12:08:10 +0900

    353

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_print_comb2(void);
+
+int main(void)
+{
+	ft_print_comb2(void);
+	return 0;
+}
+
+void ft_print_comb2()
+{
+	char i;
+	i = '0';
+	while( i < '100' );
+	{
+		write(1, %i, 1);
+		i++;
+}
